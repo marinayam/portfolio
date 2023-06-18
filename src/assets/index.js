f250c4e589365b0e58810200eb5b8dbd34ee8f01
marina <mrnymda@gmail.com>
2023-06-19 03:04:05 +0900

    skills ・boostrap_iconsからimgに変更 ・constants使用 ・レイアウトを1つのboxに変更

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,14 @@ import logo from "./logo.svg";
 import headerImg from "./header-img.svg";
 import github from "./github.png";
 import githubIcon from "./github-icon.svg";
+import codeSlash from "./code-slash.svg";
+import layoutTextWindow from "./layout-text-window.svg";
 
 export {
   logo,
   headerImg,
   github,
   githubIcon,
+  codeSlash,
+  layoutTextWindow,
 };
--- src/containers/skills/Skills.jsx
+++ src/containers/skills/Skills.jsx
@@ -1,52 +1,49 @@
 import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
-import { CodeSlash } from "react-bootstrap-icons"
-import { LayoutTextWindow } from "react-bootstrap-icons"
+import { skills } from "../../constants";
+
 import './skills.css';
+
+const SkillCard = ({
+  image,
+  title,
+  features_title,
+  experience,
+  features_subtitle,
+  tools,
+}) => {
+  return (
+    <div className='skills__item'>
+      <div className='skills-icon'>
+        <img src={image} />
+      </div>
+        <h2>{title}</h2>
+        <h4>{features_title}</h4>
+        <p>{experience}</p>
+        <h4>{features_subtitle}</h4>
+        <div>
+        <ul>{tools.map((tool) => (
+            <li>{tool.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
 
 const Skills = () => {
   
   return (
   <section className="skill" id="skills">
-    <Container>
+        <Container>
       <Row>
         <Col lg>
-          <div className="skill-bx">
-            <div className='skills-icon'>
-              <p><CodeSlash size={35} /></p>
+        <div className="skills__container">
+              {skills.map((skill, index) => (
+                <SkillCard key={`index-${index}`} index={index} {...skill} />
+              ))}
             </div>
-            <h2>Web Engineer</h2>
-            <h4>Language</h4>
-            <p>HTML, CSS, Javascript,PHP, SQL</p>
-            <h4>Flamework,Tools</h4>
-            <ul>
-              <li>React</li>
-              <li>Sass</li>
-              <li>jQuery</li>
-              <li>Bootstrap</li>
-              <li>Tailwind</li>
-              <li>Next.js</li>
-              <li>Laravel</li>
-              <li>Git</li>
-            </ul>
-          </div>
-        </Col>
-        <Col lg>
-          <div className="skill-bx gap">
-            <div className='skills-icon'>
-              <p><LayoutTextWindow size={35} /></p>
-            </div>
-            <h2>Web Designer</h2>
-            <h4>Experience</h4>
-            <p>UX, UI, Web,Thumbnail</p>
-            <h4>Tools</h4>
-            <ul>
-              <li>Figma</li>
-              <li>Wordpress</li>
-              <li>Webflow</li>
-              <li>Wix</li>
-            </ul>
-          </div>
         </Col>
       </Row>
     </Container>
