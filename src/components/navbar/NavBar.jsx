7422c796fbfd41e86e808142b04b40b8fea1878d
marina <mrnymda@gmail.com>
2023-06-25 11:04:17 +0900

    contact button link追加 homeメニュー 削除

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import {Navbar, Container, Nav } from 'react-bootstrap';
 import { logo } from "../../assets";
 import githubIcon from '../../assets/github-icon.svg'
+import { HashLink } from 'react-router-hash-link';
+import {BrowserRouter as Router } from "react-router-dom";
 import './navbar.css';
 
   const NavBar = () => {
@@ -28,40 +30,38 @@ import './navbar.css';
   }
 
   return (
-    <Navbar expand="lg" className={scrolled ? 'scrolled' : ''} >
-      <Container>
-        <Navbar.Brand href="#home">
-          <img src={logo} alt="Logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <span className='navbar-toggler-icon'></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} 
-            onclick={() => onUpdateActiveLink('home')}>
-              Home
-            </Nav.Link>
-            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}
-            onclick={() => onUpdateActiveLink('skills')}>
-              Skills
-            </Nav.Link>
-            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}
-            onclick={() => onUpdateActiveLink('projects')} >
-              Projects
-            </Nav.Link>
-          </Nav> 
-          <span className='navbar-text'>
-            <div className='social-icon'>
-              <a href='home'><img src={githubIcon} alt='' /></a>
-            </div>
-            <button className='vvd' onClick={() => console.log('connect')}>
-              <span>contact</span>
-            </button>
-          </span>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Router>
+      <Navbar expand="lg" className={scrolled ? 'scrolled' : ''} >
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className='navbar-toggler-icon'></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}
+              onclick={() => onUpdateActiveLink('skills')}>
+                Skills
+              </Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}
+              onclick={() => onUpdateActiveLink('projects')} >
+                Projects
+              </Nav.Link>
+            </Nav> 
+            <span className='navbar-text'>
+              <div className='social-icon'>
+                <a href='https://github.com/marinayam?tab=repositories'  target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt='' /></a>
+              </div>
+              <HashLink to='#contact'>
+                <button><span>contact</span></button>
+              </HashLink>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
   );
 };
 
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap"
 import { ArrowRightCircle } from "react-bootstrap-icons"
 import { headerImg } from "../../assets";
+import { HashLink } from 'react-router-hash-link';
+import {BrowserRouter as Router } from "react-router-dom";
 import './header.css';
 
   const Header = () => {
@@ -10,7 +12,6 @@ import './header.css';
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
   const toRotate = [ "Frontend Engineer", "Web Designer"];
   const period = 2000;
 
@@ -35,33 +36,33 @@ import './header.css';
 
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
       setDelta(period);
     } else if (isDeleting && updatedText === '') {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
-      setIndex(1);
       setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
     }
   }
 
   return(
     <section className="header" id="home">
-      <Container>
-        <Row className="align-items-center">
-          <Col xs={12} md={6} xl={7}>
-            <span className="tagline">welcome to my portfolio</span>
-            <h1>{"Hi I'm Marina "}<br></br><span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Frontend engineer", "Web Designer"]'><span className="wrap">{text}</span></span></h1>
-            <p>初めましてmarinaです 人々の生活を豊かにするサービスを提供するため、UI/UXを意識した設計・デザインから開発まで対応出来るように日々スキルを磨いております</p>
-            <button onClick={() => console.log('contact')}>contact<ArrowRightCircle size={25} /></button>
-          </Col>
-          <Col xs={12} md={6} xl={5}>
-            <img src={headerImg} alt="Header img" />
-          </Col>
-        </Row>
-      </Container>
+      <Router>
+        <Container>
+          <Row className="align-items-center">
+            <Col xs={12} md={6} xl={7}>
+              <span className="tagline">welcome to my portfolio</span>
+              <h1>{"Hi I'm Marina "}<br></br><span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Frontend engineer", "Web Designer"]'><span className="wrap">{text}</span></span></h1>
+              <p>初めましてmarinaです 人々の生活を豊かにするサービスを提供するため、UI/UXを意識した設計・デザインから開発まで対応出来るように日々スキルを磨いております</p>
+              <HashLink to='#contact'>
+                <button>contact<ArrowRightCircle size={25} /></button>
+              </HashLink>
+            </Col>
+            <Col xs={12} md={6} xl={5}>
+              <img src={headerImg} alt="Header img" />
+            </Col>
+          </Row>
+        </Container>
+      </Router>
     </section>
   )
 } ;
