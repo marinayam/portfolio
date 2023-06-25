56a3d3dc0f10b8e62e232637e0d819bbc2cb5922
marina <mrnymda@gmail.com>
2023-06-25 10:52:18 +0900

    contact

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 import { Header, Skills, Projects } from './containers';
-import { NavBar } from './components';
+import { NavBar, Contact } from './components';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => (
-  <div className="App">
+    <div className="App">
     <NavBar />
     <Header />
     <Skills />
     <Projects />
+    <Contact />
   </div>
-);
+  );
 
 export default App;
